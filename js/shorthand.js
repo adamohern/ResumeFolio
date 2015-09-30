3e129ab55e2e6c593b9b66060de89e035aff0eec
adamohern <adam@ChocolateChip.local>
2015-09-30 12:52:16 -0400

    Cleaning up, adding images, fancy box

--- js/shorthand.js
+++ js/shorthand.js
@@ -1,10 +1,17 @@
-function timeSince(time,format){
-    
+function yearsSince(date){
+    var prevTime = new Date(date);
+    var thisTime = new Date();
+    var diff = thisTime.getTime() - prevTime.getTime();
+    return Math.round(diff / (1000 * 60 * 60 * 24 * 365) * 10) / 10;
 }
 
 function shorthand(text){
+    DATE = new Date();
     if (typeof text == "string") { 
-        return text.replace(/\{([^\}\}]*)\}\(([^\(\)]*)\)/g,'<span class="$2">$1</span>');
+        text = text.replace(/\{([^\}]*)\}\((yearsSince)\)/g,yearsSince("$1"));
+        text = text.replace(/\*\((date)\)/g,DATE.toDateString());
+        text = text.replace(/\{([^\}]*)\}\(([^\)]*)\)/g,'<span class="$2">$1</span>');
+        return text;
     }
     else return null;
 }
